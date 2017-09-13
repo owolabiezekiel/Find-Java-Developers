fd048f6ed06fb6c8be866f417730edcd285107eb
=tobilobaezekiel <owo.ezekiel@gmail.com>
2017-09-13 20:25:01 +0100

    first commit

---
+++ app/src/main/java/com/fitn/findjavadeveloper/api/Client.java
@@ -0,0 +1,23 @@
+package com.fitn.findjavadeveloper.api;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by TOBILOBA on 9/11/2017.
+ */
+
+public class Client {
+    public static final String BASE_URL = "https://api.github.com";
+    public static Retrofit retrofit = null;
+
+    public static Retrofit getClient(){
+        if (retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    } // end of getClient method
+}// end of main class
---
+++ app/src/main/java/com/fitn/findjavadeveloper/api/Service.java
@@ -0,0 +1,15 @@
+package com.fitn.findjavadeveloper.api;
+
+import com.fitn.findjavadeveloper.model.ItemResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+/**
+ * Created by TOBILOBA on 9/11/2017.
+ */
+
+public interface Service {
+    @GET("/search/users?q=language:java+location:lagos")
+    Call<ItemResponse> getItems();
+}
